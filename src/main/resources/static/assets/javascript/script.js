f1d178c4214ca5cd6b2c94d0d32b2fe1350e4252
samreb22daw <samreb22daw@proton.me>
2023-05-10 19:36:07 +0200

    Avançant projecte

--- src/main/resources/static/assets/javascript/script.js
+++ src/main/resources/static/assets/javascript/script.js
@@ -1,10 +1,8 @@
 const ciudad = "Barcelona";
-
-// Primera petición para obtener la latitud y longitud de una ciudad
-
 let latitud;
 let longitud;
 const url = `https://geocoding-api.open-meteo.com/v1/search?name=${ciudad}`;
+let dadesOpenMeteo = [];
 
 
 fetch(url)
@@ -13,9 +11,21 @@ fetch(url)
         latitud = data.results[0].latitude.toString();
         longitud = data.results[0].longitude.toString();
 
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m&current_weather=true&timezone=auto`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m&current_weather=true&timezone=auto&daily=precipitation_probability_max`)
             .then(response => response.json())
             .then(data => {
-                console.log(data.current_weather);
+                console.log(data)
+                let tempActual = data.current_weather.temperature;
+                let weatherCode = data.current_weather.weathercode;
+                let precipitacionMax = data.daily.precipitation_probability_max[0];
+                let dia = data.current_weather.time;
+                let diaHora = dia.replace("T", " ");
+                document.getElementById('ciudad').innerHTML = ciudad;
+                document.getElementById('temperatura').innerHTML = tempActual;
+                document.getElementById('precipitacion').innerHTML = precipitacionMax;
+                document.getElementById('dia-hora').innerHTML = diaHora + "h";
+
             }).catch(error => console.log("Error obteniendo la latitud y longitud de la ciudad: ",error));
     }).catch(error => console.log("Error obteniendo la latitud y longitud de la ciudad: ",error));
+
+
